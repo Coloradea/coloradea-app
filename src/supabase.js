099721b7b52d7430763848eb159cefd49c541c33
Coloradea <antoinecorre@colorboutik.com>
2026-04-07 11:29:12 +0300

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://VOTRE_ID.supabase.co'
-const supabaseKey = 'VOTRE_ANON_KEY'
+const supabaseUrl = 'https://ttsheptdateqblcenxjb.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR0c2hlcHRkYXRlcWJsY2VueGpiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU1MzU2MzAsImV4cCI6MjA5MTExMTYzMH0.chksYwsgFHx1l4_Pqvs3KLhCKCgrkSKajlTB0hIFrHk'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
